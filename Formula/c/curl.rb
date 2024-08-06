d8434c86965101c9f70fd5a954886308c9a24215
kpcyrd <git@rxv.cc>
2024-08-06 17:34:41 +0200

    Initial commit

---
+++ Formula/c/curl.rb
@@ -0,0 +1,91 @@
+class Curl < Formula
+  desc "Get a file from an HTTP, HTTPS or FTP server (with rustls)"
+  homepage "https://curl.se"
+  # Don't forget to update both instances of the version in the GitHub mirror URL.
+  url "https://curl.se/download/curl-8.9.1.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8_9_1/curl-8.9.1.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-8.9.1.tar.bz2"
+  sha256 "b57285d9e18bf12a5f2309fc45244f6cf9cb14734e7454121099dd0a83d669a3"
+  license "curl"
+
+  livecheck do
+    url "https://curl.se/download/"
+    regex(/href=.*?curl[._-]v?(.*?)\.t/i)
+  end
+
+  head do
+    url "https://github.com/curl/curl.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  keg_only :provided_by_macos
+
+  depends_on "pkg-config" => :build
+  depends_on "brotli"
+  depends_on "ca-certificates"
+  depends_on "libidn2"
+  depends_on "libnghttp2"
+  depends_on "libssh2"
+  depends_on "rtmpdump"
+  depends_on "rustls-ffi"
+  depends_on "zstd"
+
+  uses_from_macos "krb5"
+  uses_from_macos "zlib"
+
+  def install
+    tag_name = "curl-#{version.to_s.tr(".", "_")}"
+    if build.stable? && stable.mirrors.grep(/github\.com/).first.exclude?(tag_name)
+      odie "Tag name #{tag_name} is not found in the GitHub mirror URL! " \
+           "Please make sure the URL is correct."
+    end
+
+    system "./buildconf" if build.head?
+
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-ldap
+      --disable-ldaps
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --with-rustls
+      --without-openssl
+      --with-ca-bundle=#{Formula["ca-certificates"].pkgetc/"cert.pem"}
+      --without-ca-path
+      --with-default-ssl-backend=rustls
+      --with-libidn2
+      --with-librtmp
+      --with-libssh2
+      --without-libpsl
+      --with-zsh-functions-dir=#{zsh_completion}
+      --with-fish-functions-dir=#{fish_completion}
+    ]
+
+    args << if OS.mac?
+      "--with-gssapi"
+    else
+      "--with-gssapi=#{Formula["krb5"].opt_prefix}"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+    system "make", "install", "-C", "scripts"
+    libexec.install "scripts/mk-ca-bundle.pl"
+  end
+
+  test do
+    # Fetch the curl tarball and see that the checksum matches.
+    # This requires a network connection, but so does Homebrew in general.
+    filename = (testpath/"test.tar.gz")
+    system bin/"curl", "-L", stable.url, "-o", filename
+    filename.verify_checksum stable.checksum
+
+    system libexec/"mk-ca-bundle.pl", "test.pem"
+    assert_predicate testpath/"test.pem", :exist?
+    assert_predicate testpath/"certdata.txt", :exist?
+  end
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+# homebrew-tap
+
+Third-party [Homebrew](https://brew.sh) repository for [rustls-ffi](https://github.com/rustls/rustls-ffi) related packages.
+
+## Installation
+
+```
+brew tap rustls/tap
+brew install rustls/tap/curl
+```
+
+See also [Homebrew documentation for Taps](https://docs.brew.sh/Taps).
