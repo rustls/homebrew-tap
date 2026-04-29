827b8bc6314192b9feb4f7ca84f8cfed7de8f114
Daniel McCarney <daniel@binaryparadox.net>
2026-04-29 10:24:42 -0400

    Formula: curl 8.19.0 -> 8.20.0
    See https://github.com/curl/curl/releases/tag/curl-8_20_0

--- Formula/c/curl.rb
+++ Formula/c/curl.rb
@@ -2,10 +2,10 @@ class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server (with rustls)"
   homepage "https://curl.se"
   # Don't forget to update both instances of the version in the GitHub mirror URL.
-  url "https://curl.se/download/curl-8.19.0.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-8_19_0/curl-8.19.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/curl-8.19.0.tar.bz2"
-  sha256 "eba3230c1b659211a7afa0fbf475978cbf99c412e4d72d9aa92d020c460742d4"
+  url "https://curl.se/download/curl-8.20.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8_20_0/curl-8.20.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-8.20.0.tar.bz2"
+  sha256 "4be48e69cf467246cb97d369b85d78a08528f2b37cffef2418ee16e6a4eb596e"
   license "curl"
 
   livecheck do
